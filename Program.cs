16a99ab517cb73320ff042a5494d1c66c6fb83b5
Lucas Viana <luks79180@gmail.com>
2025-10-26 19:45:49 -0300

    final version

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CsharpAD06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ApresentarPrograma();
+
+        LacoRepeticao();
+    }
+
+    public static void ApresentarPrograma()
+    {
+        Console.WriteLine("-- EXERCÍCIOS DE LAÇO DE REPETIÇÃO --");
+    }
+
+    public static void LacoRepeticao()
+    {
+        Console.WriteLine("Determine a altura do triângulo:");
+        int AlturaTriangulo = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < AlturaTriangulo; i++)
+        {
+            for (int j = 0; j < AlturaTriangulo - (i + 1); j++)
+            {
+                Console.Write("  ");
+            }
+
+            for (int k = 0; k <= i; k++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+
+    }
+}
